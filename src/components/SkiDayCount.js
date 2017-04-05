34bc802ee35f0a844d95b125d887ac2bb706d288
appyselvam <sel14soft@gmail.com>
2017-04-05 10:25:18 +0530

    react createClass syntex of component

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -2,17 +2,37 @@ import React from 'react'
 import '../css/ui.scss'
 
 export const SkiDayCount = React.createClass({
+	
+	percentToDecimal(decimal){
+		return ( (decimal*100) + '%');
+	},
+	calcGoalProgress(total, goal){
+		return this.percentToDecimal(total/goal);
+	},
 	render(){
 		return(
 			<div className="ski-day-count">
 				<div className="total-days">
-					<span>5 days</span>
+					<span>{this.props.total}</span>
+					<span> days</span>
 				</div>
 				<div className="powder-days">
-					<span>2 days</span>
+					<span>{this.props.powder}</span>
+					<span> days</span>
 				</div>
 				<div className="backcountry-days">
-					<span>1 days</span>
+					<span>{this.props.backcountry}</span>
+					<span> days</span>
+				</div>
+				<div>
+					<span>
+						{
+							this.calcGoalProgress(
+								this.props.total,
+								this.props.goal
+							)
+						}
+					</span>
 				</div>
 			</div>
 		)
